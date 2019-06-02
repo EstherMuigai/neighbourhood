a46a6f3fcafd286feca5c1515958a7560492f902
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-06-02 15:43:49 +0300

    Add User profiles.

--- moringa/views.py
+++ moringa/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import ProfileForm
+from .models import Profile
 
 def welcome(request):
     return render(request, 'welcome.html')
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
-    return render(request, 'profile.html')
+    current_user=request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()         
+    else:
+        form = ProfileForm()
+    return render(request, 'profile.html',{"form":form})
 
 def timeline(request):
     return render(request, 'timeline.html')
